297f37d109f94e32ab8359466c3fee990ed20ea9
Dong Ngo - Dev Abaha 04 <devabaha04@gmail.com>
2024-10-14 15:45:16 +0700

    make page PWA

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('app-cache').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/styles.css',
+        '/app.js',
+        'https://placehold.co/192x192',
+        'https://placehold.co/512x512',
+      ]);
+    }),
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    }),
+  );
+});
